2b300bd48e11e3123080bb7af858f99c23c53067
Thành Vũ <csnsjh@gmail.com>
2021-12-06 10:58:21 +0700

    fix: fix format price for small price

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,8 +1,19 @@
 export const formatPrice = (price: number): string => {
-  return `$${new Intl.NumberFormat('de-DE', {
-    style: 'currency',
-    currency: 'USD',
-  })
-    .format(price)
-    .slice(0, -2)}`;
+  const stringValue = price.toString();
+  const [decimal, fraction] = stringValue.split('.');
+  const formattedDecimal = new Intl.NumberFormat('de-DE').format(
+    parseInt(decimal, 10),
+  );
+
+  if (price < 0.0001) {
+    return `$${formattedDecimal},${fraction.slice(0, 6)}`;
+  } else if (price < 0.001) {
+    return `$${formattedDecimal},${fraction.slice(0, 5)}`;
+  } else if (price < 0.01) {
+    return `$${formattedDecimal},${fraction.slice(0, 4)}`;
+  } else if (price < 100) {
+    return `$${formattedDecimal},${fraction.slice(0, 3)}`;
+  } else {
+    return `$${formattedDecimal},${fraction.slice(0, 2)}`;
+  }
 };
